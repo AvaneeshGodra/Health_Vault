1362db55b09bb10c344804511eac38e42506f719
AvaneeshGodra <avaneeshgodra12@gmail.com>
2023-10-28 03:41:14 +0530

    AI printing with one click

--- frontend/src/modules/user/components/Aiconsult.js
+++ frontend/src/modules/user/components/Aiconsult.js
@@ -1,79 +1,86 @@
 import React, { useRef, useState } from 'react';
-import './NoteCard/NoteCard.css'
+import './NoteCard/NoteCard.css';
 import './style1.css';
 import { networkOperations } from '../../../shared/services/api-client';
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, CircularProgress } from '@mui/material';
 
+const SearchComponent = ({ data }) => {
+  const symptomRef = useRef('');
+  const [arrNote, setArr] = useState([]);
+  const [output, setOutput] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showOutput, setShowOutput] = useState(false);
 
-const SearchComponent = ({data}) => {
-  const symptomRef=useRef('')
-  const [arrNote,setArr]=useState([]);
-  const [output,setOutput]=useState([]);
   const consultation = {
     mid: data.Mid,
   };
-  const onclick=async()=>{
-    try {
-   
-      const response = await networkOperations.post(
-        process.env.REACT_APP_DISPLAY_CONSULTATION,
-        consultation
-      ); //backend
-       
-        const arr=response.data.message;
-        setArr(arr);
-       
-       
 
-    } catch (err) {
-      // console.log(err);
-    }
-  
-    var print=arrNote.reduce((a,n)=>
-      a+n.note
-   ,'')
-  //  console.log(print);
-    
-    const send={
-      symptoms:symptomRef.current.value,
-      diagnosis:print
-    }
+  const sendToSecondAPI = async (print) => {
     try {
+      setLoading(true); // Start loading animation
+      const send = {
+        symptoms: symptomRef.current.value,
+        diagnosis: print,
+      };
+
       const response = await networkOperations.post(
         process.env.REACT_APP_AI_MODEL,
         send
-      ); //backend
-       
-      
-        setOutput(response.data[0].candidates[0].output);
-      //  console.log(response.data[0].candidates[0].output)
+      );
 
+      setOutput(response.data[0].candidates[0].output);
+      setLoading(false); // Stop loading animation
+      setShowOutput(true);
     } catch (err) {
-      // console.log(err);
+      setLoading(false); // Stop loading animation in case of error
+      // Handle errors for the second API call
     }
-  
-  }
+  };
 
- 
-  
-  
+  const onclick = async () => {
+    try {
+      const response = await networkOperations.post(
+        process.env.REACT_APP_DISPLAY_CONSULTATION,
+        consultation
+      );
+
+      const arr = response.data.message;
+      setArr(arr);
+
+      const print = arr.reduce((a, n) => a + n.note, '');
+
+      if (print.trim() !== '') {
+        sendToSecondAPI(print);
+      }
+    } catch (err) {
+      // Handle errors for the first API call
+    }
+  };
+
   return (
     <>
-    
-
-<h1>AI Doctor</h1>
-    <label for="symptoms">Symptoms:</label>
-    <TextField id="systum" rows="4" cols="50" inputRef={symptomRef}></TextField>
-    <button id="generateButton" onClick={onclick}className='btn btn-primary'>Generate Response</button>
-    <div id="response"></div>
-    <br/><br/>
-    <label for="diagnosis">Diagnosis:</label>
-    <div id="diagnosis" rows="4" cols="50" className='NoteCard w-75'><pre>{output}</pre> </div><br/><br/>
-   
-    
- 
+      <h1>AI Doctor</h1>
+      <label htmlFor="symptoms">Symptoms:</label>
+      <TextField id="systum" rows="4" cols="50" inputRef={symptomRef}></TextField>
+      <button id="generateButton" onClick={onclick} className='btn btn-primary'>
+        Generate Response
+      </button>
+      <div id="response"></div>
+      <br /><br />
+      {loading ? ( // Show loading animation when waiting for response
+        <CircularProgress />
+      ) : (
+        <div>
+          <label htmlFor="diagnosis">Diagnosis:</label>
+          {showOutput && output && (
+            <div id="diagnosis" rows="4" cols="50" className='NoteCard w-75'>
+              <pre>{output}</pre>
+            </div>
+          )}
+        </div>
+      )}
+      <br /><br />
     </>
-
   );
 };
 
